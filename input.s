d7bbce5b28af6d73029f5da7c88a88c25ed7a440
chuan0306 <62919709+chuan0306@users.noreply.github.com>
2024-01-10 16:25:10 +0800

    Update input.s

--- input.s
+++ input.s
@@ -1,8 +1,8 @@
 .data 
-	t0_u: .word: ACE49DCF		 # upper bits of test data0
-	t0_l: .word: FC12BAF6		 # upper bits of test data0
-	t1_u: .word: EA295883		 # upper bits of test data1
-	t1_l: .word: 3D019820		 # upper bits of test data1
-	t2_u: .word: 112AB2BE		 # upper bits of test data2
-	t2_l: .word: 04BB7B06		 # upper bits of test data2
+	t0_u: .word 0xACE49DCF		 # upper bits of test data0
+	t0_l: .word 0xFC12BAF6		 # upper bits of test data0
+	t1_u: .word 0xEA295883		 # upper bits of test data1
+	t1_l: .word 0x3D019820		 # upper bits of test data1
+	t2_u: .word 0x112AB2BE		 # upper bits of test data2
+	t2_l: .word 0x04BB7B06		 # upper bits of test data2
 	TEST_DATA_NUMS: .word 3
