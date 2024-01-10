97dc3f224bcb0d9def9b641d7d38fd1eb13e4bc6
chuan0306 <62919709+chuan0306@users.noreply.github.com>
2024-01-10 09:52:14 +0800

    Add files via upload

---
+++ input.s
@@ -0,0 +1,8 @@
+.data 
+	t0_u: .word: ACE49DCF		 # upper bits of test data0
+	t0_l: .word: FC12BAF6		 # upper bits of test data0
+	t1_u: .word: EA295883		 # upper bits of test data1
+	t1_l: .word: 3D019820		 # upper bits of test data1
+	t2_u: .word: 112AB2BE		 # upper bits of test data2
+	t2_l: .word: 04BB7B06		 # upper bits of test data2
+	TEST_DATA_NUMS: .word 3
